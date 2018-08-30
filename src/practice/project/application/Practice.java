62b4aab446a12b1934ae47765aa2b202eba64d53
Ramanjaneyulu12345 <raman.sriram1@gmail.com>
2018-08-30 20:31:38 +0530

    First Commit

---
+++ src/practice/project/application/Practice.java
@@ -0,0 +1,9 @@
+package practice.project.application;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		System.out.println ("STS Done, GIT in progress - 30/8/2018");
+	}
+
+}
