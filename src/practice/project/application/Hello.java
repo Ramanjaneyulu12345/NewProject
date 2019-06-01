61cd2ac035932b19f6d8f9756ad13ef993501384
karthee145 <tkarthik.askme@gmail.com>
2019-06-01 08:05:14 +0530

    updated files

--- src/practice/project/application/Hello.java
+++ src/practice/project/application/Hello.java
@@ -3,7 +3,7 @@ package Hello;
 public class HelloClass {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World STS121123123113213213");
+		System.out.println("Hello World STS");
 	}
 
 }
